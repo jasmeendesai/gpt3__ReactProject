8443da0a400997237078afcae1ade9c709bc1d86
jasmeendesai <jasmeendesai2597@gmail.com>
2023-09-25 12:33:44 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
+
+import {Brand, Navbar, CTA} from './components'
+import {Blog, Footer, Features, Possibility, WhatGpt3, Header} from './containers'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGpt3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
